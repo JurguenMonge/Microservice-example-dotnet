a06104b81088025b7ca0afc2bedca38c27b7fdf9
Jurguen Monge Rojas <82221622+JurguenMonge@users.noreply.github.com>
2024-11-17 19:03:23 -0600

    cambios

--- OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
+++ OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+740a4437eca33ea960b0c52d193bc71ba9ce2499")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
